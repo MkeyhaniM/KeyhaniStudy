d2a163e0928944b65fb6eb4b7b74a97598d76799
mkeyhanim <mkeyhanideve@gmail.com>
2023-02-25 11:59:47 -0800

    Pecked Code for responsive page

---
+++ assets/dist/js/responsive.js
@@ -0,0 +1,9 @@
+$(document).ready(e => {
+    $("#barNabigation").click(e => {
+        if (!$('section.container-fluid section#allWidthHeight nav#navigator div:first-child').hasClass("responsive")) {
+            $('section.container-fluid section#allWidthHeight nav#navigator div:first-child').addClass('responsive')
+        } else {
+            $('section.container-fluid section#allWidthHeight nav#navigator div:first-child').removeClass('responsive')
+        }
+    })
+})
